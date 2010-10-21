ca4b3a577dd6fcc1d78ab7fd56e8680a46f9afd8
Jared Martin <jaredtmartin@gmail.com>
2010-10-21 13:29:52 -0600

    sample apache conf

---
+++ branch.py
@@ -0,0 +1,8 @@
+from settings import *
+
+DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'jade'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'root'             # Not used with sqlite3.
+DATABASE_PASSWORD = 't1bur0n'         # Not used with sqlite3.
+
+SITE_ID = 2
