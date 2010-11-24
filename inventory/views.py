c57463def6b7effb0d055a4c3afc12b966c85546
Jared Martin <jaredtmartin@gmail.com>
2010-11-23 23:18:29 -0600

    added counts and production and added license

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,18 @@
+# Jade Inventory Control System
+#Copyright (C) 2010  Jared T. Martin
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied account of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from jade.inventory.models import Count, Sale, Purchase, INVENTORY_ACCOUNT, CASH_ACCOUNT, REVENUE_ACCOUNT, TAX_ACCOUNT, EXPENSE_ACCOUNT
 from jade.inventory.forms import *
 #from django.template import loader, Context, RequestContext
--- manual/urls.py
+++ manual/urls.py
@@ -1,3 +1,18 @@
+# Jade Inventory Control System
+#Copyright (C) 2010  Jared T. Martin
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied account of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
@@ -7,6 +22,8 @@ urlpatterns = patterns('',
     (r'^home/$', direct_to_template, {'template': 'manual/home.html'}),
     (r'^accounts/$', direct_to_template, {'template': 'manual/accounts.html'}),
     (r'^transactions/$', direct_to_template, {'template': 'manual/transactions.html'}),
-    (r'^purchases/$', direct_to_template, {'template': 'manual/purchases.html'}),
+    (r'^counts/$', direct_to_template, {'template': 'manual/counts.html'}),
+    (r'^production/$', direct_to_template, {'template': 'manual/production.html'}),
+    (r'^$', direct_to_template, {'template': 'manual/home.html'}),
 )
 
