f77a4acaf8cd7ea57900e8fe6a2a88fd85498a10
Jared Martin <jaredtmartin@gmail.com>
2010-11-26 10:32:54 -0600

    but zero

--- inventory/models.py
+++ inventory/models.py
@@ -161,7 +161,7 @@ class Item(models.Model):
         # Returns the cost of the item NOT including the cost of any linked items
         # this should be used as the cost on a sale
         stock=self.stock
-#        if not stock or stock<=0: return self.default_cost
+        if not stock or stock==0: return self.default_cost
         return self.total_cost/stock
     individual_cost=property(_get_individual_cost)
     
