b8bc400ae574e0af6aa3ac758366899cef3b0e25
Jared Martin <jaredtmartin@gmail.com>
2010-12-25 17:04:35 -0600

    cart stores price

--- cart/models.py
+++ cart/models.py
@@ -1,20 +1,22 @@
 from django.db import models
 from jade.inventory.models import Item
+from django.contrib.sessions.models import Session
+
 
 class CartItem(models.Model):
-    cart_id = models.CharField(max_length=50)
+    session = models.ForeignKey(Session)
     date_added = models.DateTimeField(auto_now_add=True)
     quantity = models.IntegerField(default=1)
-    product = models.ForeignKey(Item, unique=False)
+    item = models.ForeignKey(Item, unique=False)
     class Meta:
         db_table = 'cart_items'
         ordering = ['date_added']
     def total(self):
-        return self.quantity * self.product.price
+        return self.quantity * self.item.price(Setting.get('Default client'))
     def name(self):
-        return self.product.name
+        return self.item.name
     def price(self):
-        return self.product.price
+        return self.item.price
     def get_absolute_url(self):
         return self.product.get_absolute_url()
     def augment_quantity(self, quantity):
